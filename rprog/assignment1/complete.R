66118387eb21c944a78ae446ff15c8cf20431253
Fredrik Rönnvall <fredronn@gmail.com>
2014-07-14 19:44:07 +0100

    Fix complete function Add skeleton corr function
    Add workdata

--- rprog/assignment1/complete.R
+++ rprog/assignment1/complete.R
@@ -13,15 +13,19 @@ complete <- function(directory, id = 1:332) {
   ## where 'id' is the monitor ID number and 'nobs' is the
   ## number of complete cases
   files <- list.files(path = directory, full.names = TRUE)
-  data <- do.call("rbind", lapply(files,read.csv, header = TRUE)) 
-  complete_subset <- subset(data, !is.na(sulfate) & !is.na(nitrate) & ID %in% id)
-  return_data <- aggregate(complete_subset['ID'], by = complete_subset['ID'], length)
-  names(return_data) <- c("id", "nobs")
-  rd = NULL
+  data <- do.call("rbind", lapply(files,read.csv, header = TRUE))
+  r_data <- NULL
   for (i in id){
-    rd <- rbind(rd, c(i,return_data[return_data$id==i, 2]))
+    s_data <- subset(data, ID == i)
+    n = nrow(s_data)
+    if ( n > 0 ){
+      n_complete <- nrow(subset(s_data, !is.na(sulfate) & !is.na(nitrate)))
+      r_data <- rbind(r_data, c(i, n_complete))
+    }
   }
-  colnames(rd) <- c("id","nobs")
-  row.names(rd) <- 1:length(id)
-  return(data.frame(rd))
+  
+  if ( !is.null(r_data) ){
+    colnames(r_data) <- c("id", "nobs")
+  }
+  return(data.frame(r_data))
 }
